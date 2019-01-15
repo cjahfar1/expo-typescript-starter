cc87cb42c65e0e037c7dcd2f12bdf2833eac73e3
Bhaskar Murthy <bhaskar@murthy.ca>
2019-01-14 20:31:35 -0500

    Setup react-navigation

--- App.tsx
+++ App.tsx
@@ -1,15 +1,39 @@
 import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { useScreens } from 'react-native-screens'
+import {
+  createAppContainer,
+  createStackNavigator,
+  NavigationScreenProps,
+} from 'react-navigation'
 
-export default class App extends React.Component {
+useScreens()
+
+class App extends React.Component<NavigationScreenProps> {
+  static navigationOptions = {
+    title: 'Home',
+  }
+
+  _push = () => {
+    this.props.navigation.navigate('Details')
+  }
+
   public render() {
     return (
       <View style={styles.container}>
         <Text>Hello, typescript</Text>
+        <Button onPress={this._push} title="Push" />
       </View>
     )
   }
 }
+
+const AppNavigator = createStackNavigator({
+  Home: App,
+  Details: App,
+})
+
+export default createAppContainer(AppNavigator)
 
 const styles = StyleSheet.create({
   container: {
