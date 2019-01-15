892d99f49f82f31d45a0719d15eea1639f1e2160
Bhaskar Murthy <bhaskar@secretmission.io>
2019-01-15 10:42:22 -0500

    Add workaround for flow type + babel issue

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,14 @@ module.exports = function(api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['@babel/plugin-proposal-class-properties'],
+    plugins: [
+      '@babel/plugin-transform-flow-strip-types',
+      [
+        '@babel/plugin-proposal-class-properties',
+        {
+          loose: true,
+        },
+      ],
+    ],
   }
 }
